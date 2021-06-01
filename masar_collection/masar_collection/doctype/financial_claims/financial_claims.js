d37bbc7f3948679e8246b1022ed19fc8be9df046
karamakcsc <karamakcsc@outlook.com>
2021-06-02 01:03:00 +0300

    FirstUpload

---
+++ masar_collection/masar_collection/doctype/financial_claims/financial_claims.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, KCSC and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Financial Claims', {
+	// refresh: function(frm) {
+
+	// }
+});
